df30a4cdff1a1a227fb25e2220053592c8681f80
jUMbooooo <jumbo.baceball0915@gmail.com>
2023-05-21 09:44:53 +0900

    add firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sales_management_app/firebase_options.dart';
 import 'add_inventory.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
   runApp(const MyApp());
 }
 
