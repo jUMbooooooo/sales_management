c3d9e786a00553b84e878c836c45ae96b3a820b6
jUMbooooo <jumbo.baceball0915@gmail.com>
2023-05-31 14:23:21 +0900

    update signin_page.dart

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -11,11 +11,23 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  Future<void> signInWithGoogle() async {
-    // GoogleSignIn をして得られた情報を Firebase と関連づけることをやっています。
-    final googleUser =
-        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+  // Future<void> signInWithGoogle() async {
+  //   // GoogleSignIn をして得られた情報を Firebase と関連づけることをやっています。
+  //   final googleUser =
+  //       await GoogleSignIn(scopes: ['profile', 'email']).signIn();
 
+  //   final googleAuth = await googleUser?.authentication;
+
+  //   final credential = GoogleAuthProvider.credential(
+  //     accessToken: googleAuth?.accessToken,
+  //     idToken: googleAuth?.idToken,
+  //   );
+
+  //   await FirebaseAuth.instance.signInWithCredential(credential);
+  // }
+  Future<UserCredential?> signInWithGoogle() async {
+    // GoogleSignIn をして得られた情報を Firebase と関連づけることをやっています。
+    final googleUser = await GoogleSignIn(scopes: ['prfile', 'email']).signIn();
     final googleAuth = await googleUser?.authentication;
 
     final credential = GoogleAuthProvider.credential(
@@ -23,7 +35,8 @@ class _SignInPageState extends State<SignInPage> {
       idToken: googleAuth?.idToken,
     );
 
-    await FirebaseAuth.instance.signInWithCredential(credential);
+    //ここでFirebaseの認証結果を返す
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   @override
@@ -41,11 +54,12 @@ class _SignInPageState extends State<SignInPage> {
           ),
           child: const Text('Googleサインイン'),
           onPressed: () async {
-            await signInWithGoogle();
+            final userCredential = await signInWithGoogle();
             // ログインが成功すると FirebaseAuth.instance.currentUser にログイン中のユーザーの情報が入ります
             // print(FirebaseAuth.instance.currentUser?.displayName);
 
-            if (mounted) {
+            if (userCredential != null && mounted) {
+              // ユーザーが正常に認証され、ユーザー情報が得られたページへ遷移
               Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(builder: (cotext) {
                   return const InventoryPage();
